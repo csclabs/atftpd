1e43e65c2212d56ec6bdb3cd230ef8abbc493b3b
Joseph Callen <jcpowermac@gmail.com>
2016-02-12 12:45:06 -0500

    Changes to wget parameters in run.sh to retry
    In our Ansible automation we run a check before executing
    the Hanlon container to determine if it is up and running before
    launching the atftpd container.  This change will allow the removal
    of that task and provide us the ability to use `docker-compose` in addition
    to continuing to use Ansible docker automation.

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-wget -q -O /tftpboot/hanlon.ipxe ${HANLON_PORT/tcp/http}/hanlon/api/v1/config/ipxe
+wget --retry-connrefused --waitretry=5 --read-timeout=5 --timeout=5 -t 10 -q -O /tftpboot/hanlon.ipxe ${HANLON_PORT/tcp/http}/hanlon/api/v1/config/ipxe
+
+if [ $? -ne 0 ]
+then 
+  echo "Unable to download iPXE script from Hanlon container"
+  exit 1
+fi
+
 chmod -R 700 /tftpboot
 chown -R nobody:nogroup /tftpboot/
 /usr/sbin/atftpd --user nobody.nogroup --daemon --no-fork --port 69 --logfile /dev/stdout /tftpboot
